4c44d1b9e0401addbd4f63e6e638127c78d771d5
Greg Thoma <CrimpReaper@gmail.com>
2023-10-15 22:13:50 -0400

    questions answered

---
+++ app.js
@@ -0,0 +1,23 @@
+//var PI = 3.14;
+//PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+// ## **Quiz**
+
+// - What is the difference between ***var*** and ***let***?
+// var will allow you to redeclare, but let will not. 
+
+// - What is the difference between **var** and ***const***?
+// var will let you reassign and redeclare the variable, const will not.
+
+// - What is the difference between **let** and **const**?
+// let allows you to reassign a variable, and const does not. 
+
+// - What is hoisting?
+// function declarations that are invoked before they have been defined. 
+// also, variables are treated as if they're at the top of code they are in. 
+
+
+
+
